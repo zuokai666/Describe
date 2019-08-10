6c9310c5e61bc95985698332e21c12ff35e13b77
zuokai666 <13703391897@163.com>
2019-08-10 13:08:04 +0800

    add problem

--- DSA/src/test/java/com/lintcode/_190710/Interval.java
+++ DSA/src/test/java/com/lintcode/_190710/Interval.java
@@ -1,7 +1,7 @@
 package com.lintcode._190710;
 
 public class Interval {
-	      int start, end;
+	public int start, end;
 	      Interval(int start, int end) {
 	          this.start = start;
 	          this.end = end;
---
+++ DSA/src/test/java/com/lintcode/_190810/Solution.java
@@ -0,0 +1,26 @@
+package com.lintcode._190810;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.lintcode._190710.Interval;
+
+public class Solution {
+	
+	public boolean canAttendMeetings(List<Interval> intervals) {
+		Collections.sort(intervals,new Comparator<Interval>() {
+
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				return o1.start - o2.start;
+			}
+		});
+		for(int i=0;i<intervals.size()-1;i++){
+			if(intervals.get(i).end > intervals.get(i+1).end){
+				return false;
+			}
+		}
+		return true;
+    }
+}
