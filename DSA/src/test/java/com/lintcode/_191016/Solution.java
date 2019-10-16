fd3205c44787f450356fbe45642fd3b0a0426a0b
zuokai666 <13703391897@163.com>
2019-10-16 15:28:29 +0800

    add today update.

---
+++ DSA/src/test/java/com/lintcode/_191016/Solution.java
@@ -0,0 +1,17 @@
+package com.lintcode._191016;
+
+public class Solution {
+
+	public int leastInterval(char[] tasks, int n) {
+		if (tasks == null || tasks.length == 0)
+			return -1;
+		if (n == 0 || tasks.length == 1)
+			return tasks.length;
+		//先统计任务种类及其数量
+		@SuppressWarnings("unused")
+		int typeNum = 0;
+		
+		
+		return n;
+	}
+}
--- describe/README.md
+++
@@ -1,4 +0,0 @@
-include "README201905.txt";<br/>
-include "boot.env.txt";<br/>
-include "README201906.txt";<br/>
-include "README201907.AQS.txt"<br/>
